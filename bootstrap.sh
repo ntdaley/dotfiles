5f37ae59861ef8e67e3d616adb8ebf56b6bd7c6b
ntdaley <ntdaley@ntdaley.org>
2015-01-19 19:56:35 +0000

    Ignore *~ files created by emacs when copying files into the home folder.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ git pull origin master
 git submodule foreach git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude "install" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude '*~' -av --no-perms . ~
 	source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
