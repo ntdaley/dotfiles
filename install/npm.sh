bf99409d687033decc58b74e5b563ebc5f9342ee
ntdaley <ntdaley@ntdaley.org>
2015-04-18 04:26:40 +0100

    Install npm using nvm, install jsmin

--- install/npm.sh
+++ install/npm.sh
@@ -1,6 +1,18 @@
+export PROFILE=~/.bash/nvm
+export NVM_DIR=~/.nvm
+mkdir -p $NVM_DIR
+touch $PROFILE
+curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
+unset PROFILE
+. ~/.bash/nvm
+
+nvm install stable
+nvm use stable
+
 npm install -g bower
 npm install -g grunt-cli
 npm install -g gulp
 npm install -g http-server
+npm install -g jsmin
 npm install -g pakmanager
 npm install -g yo
